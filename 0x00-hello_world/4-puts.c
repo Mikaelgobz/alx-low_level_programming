5569b45cf8091ddfa5fc7a356c4e4280c4f377cf
“Mikael <Liamgobz@gmail.com>
2023-02-16 11:42:34 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline"
- * Return: Always 0.
+ * Return: 0 (Success)
  */
-
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }	
 
 
